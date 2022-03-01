ac4091dc627ec2a5e8ca6eda06d767095711967a
SheikhSoltani <amir53674268@gmail.com>
2022-03-01 21:56:16 +0600

    add project

---
+++ Assets/My Project/Scripts/Root.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Root : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _camera;
+
+    [SerializeField]
+    private SpriteRenderer _background;
+    
+    [SerializeField]
+    private CharacterView _characterView;
+
+    [SerializeField]
+    private SpriteAnimationConfig _spriteAnimationConfig;
+
+    private ParalaxManager _paralaxManager;
+    private SpriteAnimator _spriteAnimator;
+
+    private void Start()
+    {
+        _paralaxManager = new ParalaxManager(_camera, _background.transform);
+        
+        _spriteAnimator = new SpriteAnimator(_spriteAnimationConfig);
+        _spriteAnimator.StartAnimation(_characterView.SpriteRenderer, Track.idle, true, 10);
+    }
+
+    private void Update()
+    {
+        _paralaxManager.Update();
+        _spriteAnimator.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Run"))
+        {
+             _spriteAnimator.StartAnimation(_characterView.SpriteRenderer, Track.run, true, 10);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
---
+++ Assets/My Project/Scripts/Track.cs
@@ -0,0 +1,5 @@
+﻿public enum Track
+{
+    idle,
+    run
+}
